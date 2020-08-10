59fd4eacbf9ee1d65d6a634576a4d232bc8e88f6
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-10 16:58:17 +1200

    adds database connection

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'natsuki',
+  password: '1234',
+  database: 'netball'
+});
+connection.connect();
+
+connection.query('SELECT * FROM players WHERE players.team=1', (err, rows, fields) => {
+    if (err) throw err;
+    console.log(rows);
+});
+
+connection.end();
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
