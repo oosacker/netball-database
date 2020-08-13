45d399966264c8401dedaeb26494cd37d1945ad6
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-13 17:57:21 +1200

    print out the names from database

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+let player_data = [];
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'natsuki',
   password: '1234',
@@ -15,9 +17,12 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-connection.query('SELECT * FROM players WHERE players.team=1', (err, rows, fields) => {
+connection.query('SELECT * FROM players WHERE team=1', (err, rows, fields) => {
     if (err) throw err;
-    console.log(rows);
+    // console.log(rows);
+
+    player_data = rows;
+    
 });
 
 connection.end();
@@ -25,7 +30,12 @@ connection.end();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.render('home');
+    console.log(player_data);
+    res.render('home', player_data);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
